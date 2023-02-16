79f3ebd8db47e76726dbc5740a5ed1e36b7a0f10
tima190 <timofejgrehov0624@gmail.com>
2023-02-16 20:49:58 +0800

    2 commit

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f"good started! We have logged in as {client.user}")
 
 @client.event
 async def on_message(message):
@@ -24,6 +24,4 @@ async def on_message(message):
 
 client.run(str(token2))
 
-@client.event
-async def on_ready():
-    print()
+
