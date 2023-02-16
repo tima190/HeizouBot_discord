8d4a0f2ae6611d4f135890e31c6e521acf045eb5
Timofey Grechka <orehovtimofej545@gmail.com>
2023-02-16 23:57:51 +0800

    now he hug you)

--- bot.py
+++ bot.py
@@ -1,5 +1,8 @@
 
 import discord
+from discord.ext import commands
+from discord import message, User
+import random
 
 token1 = open('token.txt') # create file token.txt for your token
 token2 = token1.read()
@@ -21,6 +24,19 @@ async def on_message(message):
 
     if message.content.startswith('$hello'):
         await message.channel.send('hello')
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('test'):
+        await message.channel.send(f'hello {message.author.id}')
+
+    if message.content.startswith('!hug'):
+        await message.channel.send(f"hugs {message.author.mention}")
+
 
 client.run(str(token2))
 
