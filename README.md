6cda665f4f193bf262610aa1394024430a852cd1
Timofey Grechka <orehovtimofej545@gmail.com>
2023-02-23 18:44:44 +0800

    bot completely redesigned, NOT TESTED

--- README.md
+++ README.md
@@ -12,3 +12,11 @@
 
 <img src="https://i.pinimg.com/564x/d2/f1/a6/d2f1a664e6085f90e911d263864b1fd2.jpg" align="center" alt="heizou lol">
 
+
+---
+
+<h1 align="center">install </h2>
+
+<h2>you need isntall: </h2>
+
+discord.py
--- bot.py
+++ bot.py
@@ -1,47 +1,91 @@
-
 import discord
 from discord.ext import commands
-from discord import message, User
 import random
+
+description = '''XD'''
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='?', description=description, intents=intents)
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+@bot.event
+async def on_ready():
+    print(bcolors.OKGREEN + f'Logged in as {bot.user} (ID: {bot.user.id})' + bcolors.ENDC)
+    print('------')
+
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("pong")
+
+
+@bot.command()
+async def roll(ctx, dice: str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
+
+
+@bot.command(description='For when you wanna settle the score some other way')
+async def choose(ctx, *choices: str):
+    """Chooses between multiple choices."""
+    await ctx.send(random.choice(choices))
+
+
+@bot.command()
+async def repeat(ctx, times: int, content='repeating...'):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await ctx.send(content)
+
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+
+@bot.group()
+async def cool(ctx):
+    """Says if a user is cool.
+    In reality this just checks if a subcommand is being invoked.
+    """
+    if ctx.invoked_subcommand is None:
+        await ctx.send(f'No, {ctx.subcommand_passed} is not cool')
+
+
+@cool.command(name='bot')
+async def _bot(ctx):
+    """Is the bot cool?"""
+    await ctx.send('Yes, the bot is cool.')
 
 token1 = open('token.txt') # create file token.txt for your token
 token2 = token1.read()
 token1.close
 
-intents = discord.Intents.default()
-intents.message_content = True
-
-client = discord.Client(intents=intents)
-
-@client.event
-async def on_ready():
-    print(f"good started! We have logged in as {client.user}")
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('hello')
-
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('test'):
-        await message.channel.send(f'hello {message.author.id}')
-
-    if message.content.startswith('!hug'):
-        await message.channel.send(f"hugs {message.author.mention}")
-
-    if message.content.startswith('random'):
-        rand = random.randint(1,100)
-        await message.channel.send(f"ok, is {rand}")
-
-
-client.run(str(token2))
-
-
+bot.run(str(token2))
